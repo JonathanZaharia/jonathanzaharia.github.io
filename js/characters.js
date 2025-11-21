6a90afa95f72488be9b7382d1737e4b315ca18d9
Jonathan Zaharia <jzaharia@ggc.edu>
2025-11-21 17:19:43 -0500

    Update characters.js

--- js/characters.js
+++ js/characters.js
@@ -1,9 +1,7 @@
-// characters.js
 const gremlin = document.getElementById('gremlin');
 
 gremlin.addEventListener('click', () => {
   gremlin.style.animation = 'hop 0.5s ease-out';
   setTimeout(() => gremlin.style.animation = 'hop 3s infinite', 500);
-  // Add dramatic "error" sound or visual here
+  triggerStageEvent('👾 Gremlin jumps dramatically!');
 });
-
