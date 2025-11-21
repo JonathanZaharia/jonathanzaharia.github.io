875047ac98491dbeb722d42f8e0514377ec19c25
Jonathan Zaharia <jzaharia@ggc.edu>
2025-11-21 18:14:50 -0500

    Update soundboard.js

--- js/soundboard.js
+++ js/soundboard.js
@@ -1,6 +1,1 @@
-function playWinnerSound(){
-  const ctx=new (window.AudioContext||window.webkitAudioContext)();
-  const o=ctx.createOscillator(); o.type='square';
-  o.frequency.setValueAtTime(500+Math.random()*300,ctx.currentTime);
-  o.connect(ctx.destination); o.start(); o.stop(ctx.currentTime+0.2);
-}
+
